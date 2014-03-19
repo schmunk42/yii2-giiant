8d0da574859ef08d6432aa8aafe0addcc12b92d4
Tobias Munk <schmunk@usrbin.de>
2014-03-19 15:58:03 +0100

    updated layout

--- crud/default/views/_form.php
+++ crud/default/views/_form.php
@@ -4,7 +4,7 @@ use yii\helpers\Inflector;
 use yii\helpers\StringHelper;
 
 /**
- * @var yii\web\View $this
+ * @var yii\web\View                      $this
  * @var yii\gii\generators\crud\Generator $generator
  */
 
@@ -12,7 +12,7 @@ use yii\helpers\StringHelper;
 $model = new $generator->modelClass;
 $safeAttributes = $model->safeAttributes();
 if (empty($safeAttributes)) {
-	$safeAttributes = $model->getTableSchema()->columnNames;
+    $safeAttributes = $model->getTableSchema()->columnNames;
 }
 
 echo "<?php\n";
@@ -22,23 +22,41 @@ use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 
 /**
- * @var yii\web\View $this
- * @var <?= ltrim($generator->modelClass, '\\') ?> $model
- * @var yii\widgets\ActiveForm $form
- */
+* @var yii\web\View $this
+* @var <?= ltrim($generator->modelClass, '\\') ?> $model
+* @var yii\widgets\ActiveForm $form
+*/
 ?>
 
 <div class="<?= Inflector::camel2id(StringHelper::basename($generator->modelClass)) ?>-form">
 
-	<?= "<?php " ?>$form = ActiveForm::begin(); ?>
+    <?= "<?php " ?>$form = ActiveForm::begin(); ?>
 
-<?php foreach ($safeAttributes as $attribute) {
-	echo "\t\t<?= " . $generator->generateActiveField($attribute) . " ?>\n\n";
-} ?>
-		<div class="form-group">
-			<?= "<?= " ?>Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
-		</div>
+    <div class="form-group">
+        <?= "<?= " ?>Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ?
+        'btn btn-success' : 'btn btn-primary']) ?>
+    </div>
 
-	<?= "<?php " ?>ActiveForm::end(); ?>
+    <div class="row">
+        <div class="col-md-7">
+            <?php foreach ($safeAttributes as $attribute) {
+                echo "\t\t<?= " . $generator->generateActiveField($attribute) . " ?>\n\n";
+            } ?>
+        </div>
+        <div class="col-md-3">
+            <?php foreach ($generator->getModelRelations() as $i => $relation) {
+                #var_dump($relation);
+                echo "\t\t{$relation->modelClass} {$relation->multiple}";/*<?= " . $generator->generateActiveField($attribute) . " ?>\n\n";*/
+            } ?>
+            Relations (tbd)
+        </div>
+    </div>
+
+    <div class="form-group">
+        <?= "<?= " ?>Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ?
+        'btn btn-success' : 'btn btn-primary']) ?>
+    </div>
+
+    <?= "<?php " ?>ActiveForm::end(); ?>
 
 </div>
