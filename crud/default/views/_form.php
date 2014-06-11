27fc0bdcdd219d5c49c04848488129a0c8c7b57b
Tobias Munk <schmunk@usrbin.de>
2014-06-11 23:40:54 +0200

    reverted doubled code

--- crud/default/views/_form.php
+++ crud/default/views/_form.php
@@ -87,68 +87,9 @@ EOS;
     );
     ?>";
     ?>
-        <?= "<?php "?>if (!$model->isNewRecord) { ?>
-        <?= "<?= " ?>Html::a('View', ['view', 'id'=>\Yii::$app->request->getQueryParam('id')], ['class' => 'btn btn-default']) ?>
-        <?= "<?php "?>} else {?>
-        <?= "<?= " ?>Html::a('Manage', ['index'], ['class' => 'btn btn-inverted']) ?>
-        <?= "<?php "?>} ?>
-    </div>
-
-    <?php echo "<?php \$this->beginBlock('main'); ?>"; ?>
-    <?php foreach ($safeAttributes as $attribute) {
-        echo "\t\t<?= " . $generator->generateActiveField($attribute) . " ?>\n\n";
-    } ?>
-    <?php echo "<?php \$this->endBlock(); ?>"; ?>
-
-    <?php
-    $label = 'main';
-
-    $items = <<<EOS
-[
-    'label'   => '$label',
-    'content' => \$this->blocks['main'],
-    'active'  => true,
-],
-EOS;
-    ?>
-
-    <?php foreach ($generator->getModelRelations() as $name => $relation) {
-
-        if (!$relation->multiple) {
-            continue;
-        }
-
-        echo "<?php \$this->beginBlock('$name'); ?>";
-
-        echo "<h3><?= \\yii\\helpers\\Html::a('$name', ['" . $generator->generateRelationTo(
-                                                                       $relation
-            ) . "/index']) ?></h3>";
-
-        echo "<?php echo ".$generator->generateRelationField([$relation,$name])." ?>";
-
-        echo "<?php \$this->endBlock(); ?>";
-
-        $items .= <<<EOS
-[
-    'label'   => '$name',
-    'content' => \$this->blocks['$name'],
-    'active'  => false,
-],
-EOS;
-    } ?>
-
-    <?=
-    "<?=
-    \yii\bootstrap\Tabs::widget(
-                 [
-                     'items' => [ $items ]
-                 ]
-    );
-    ?>";
-    ?>
 
     <div class="form-group">
-        <?= "<?= " ?>Html::submitButton($model->isNewRecord ? 'Create' : 'Save', ['class' => $model->isNewRecord ?
+        <?= "<?= " ?>Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ?
         'btn btn-success' : 'btn btn-primary']) ?>
     </div>
 
