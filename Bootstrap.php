d1f9f68627c092b41a2d0d78b9ba4e223f67e4eb
Tobias Munk <schmunk@usrbin.de>
2014-08-01 02:45:08 +0200

    added application bootstrap class

---
+++ Bootstrap.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @link http://www.diemeisterei.de/
+ * @copyright Copyright (c) 2014 diemeisterei GmbH, Stuttgart
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace schmunk42\giiant;
+
+use yii\base\Application;
+use yii\base\BootstrapInterface;
+
+
+/**
+ * Class Bootstrap
+ * @package schmunk42\giiant
+ * @author Tobias Munk <tobias@diemeisterei.de>
+ */
+class Bootstrap implements BootstrapInterface
+{
+
+    /**
+     * Bootstrap method to be called during application bootstrap stage.
+     *
+     * @param Application $app the application currently running
+     */
+    public function bootstrap($app)
+    {
+        if ($app->hasModule('gii')) {
+            $app->getModule('gii')->generators[] = 'schmunk42\giiant\model\Generator';
+            $app->getModule('gii')->generators[] = 'schmunk42\giiant\crud\Generator';
+            if ($app instanceof \yii\console\Application) {
+                $app->controllerMap['giiant-batch'] = 'schmunk42\giiant\commands\BatchController';
+            }
+        }
+     }
+}
