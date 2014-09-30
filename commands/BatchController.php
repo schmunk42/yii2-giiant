cbe9ebfc78dbe18bdf728daa2b4f51eb67eb56f2
Tobias Munk <schmunk@usrbin.de>
2014-09-30 23:26:29 +0200

    fixed missing crudProviders option

--- commands/BatchController.php
+++ commands/BatchController.php
@@ -61,6 +61,7 @@ class BatchController extends Controller
                 'modelBaseClass',
                 'crudControllerNamespace',
                 'crudViewPath',
+                'crudProviders',
                 'crudBaseControllerClass'
             ]
         );
