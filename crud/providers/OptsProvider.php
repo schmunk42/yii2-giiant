d0178326bf9e7fc9e7022c9dce43e42590a6d833
Uldis Nelsons <uldisnelsons@gmail.com>
2015-03-22 15:41:20 +0200

    Implemented enum in: - base model - crud( by provider OptsProvider)

--- crud/providers/OptsProvider.php
+++ crud/providers/OptsProvider.php
@@ -15,8 +15,12 @@ class OptsProvider extends \schmunk42\giiant\base\Provider
         $column     = $this->generator->getTableSchema()->columns[$attribute->name];
         $modelClass = $this->generator->modelClass;
         $func       = 'opts' . str_replace("_", "", $column->name);
-
+        $camel_func = 'opts' . str_replace(' ', '', ucwords(implode(' ', explode('_', $column->name))));
+        
         if (method_exists($modelClass::className(), $func)) {
+            $mode = isset($this->columnNames[$attribute->name]) ? $this->columnNames[$attribute->name] : null;
+        }elseif (method_exists($modelClass::className(), $camel_func)) {
+            $func = $camel_func;
             $mode = isset($this->columnNames[$attribute->name]) ? $this->columnNames[$attribute->name] : null;
         } else {
             return null;
@@ -59,4 +63,58 @@ EOS;
         return null;
 
     }
+    
+    /**
+     * Formatter for detail view attributes, who have get[..]ValueLabel function
+     * @param $column ColumnSchema
+     * @return null|string
+     */
+    public function attributeFormat($column)
+    {
+        $modelClass = $this->generator->modelClass;
+        $camel_func = 'get' . str_replace(' ', '', ucwords(implode(' ', explode('_', $column->name)))).'ValueLabel';
+        
+        if (!method_exists($modelClass::className(), $camel_func)) {
+            return null;
+        }
+
+        return <<<EOS
+            [
+                'attribute'=>'{$column->name}',
+                'value'=>{$modelClass}::{$camel_func}(\$model->{$column->name}),
+            ]
+EOS;
+    }    
+    
+    /**
+    * Formatter for detail view attributes, who have get[..]ValueLabel function
+     *
+     * @param $column ColumnSchema
+     * @param $model ActiveRecord
+     *
+     * @return null|string
+     */
+    public function columnFormat($column, $model)
+    {
+        $modelClass = $this->generator->modelClass;
+        $camel_func = 'get' . str_replace(' ', '', ucwords(implode(' ', explode('_', $column->name)))).'ValueLabel';
+        
+        if (!method_exists($modelClass::className(), $camel_func)) {
+            return null;
+        }
+
+        return <<<EOS
+			[
+                'attribute'=>'{$column->name}',
+                'value' => function (\$model) {
+                    return {$modelClass}::{$camel_func}(\$model->{$column->name});
+                }    
+            ]        
+EOS;
+
+
+
+        
+    }
+    
 }
