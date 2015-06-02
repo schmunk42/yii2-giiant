a1bcd6b02beabd79dc40db2ed7602e64182b4552
Brett O'Donnell <cornernote@gmail.com>
2015-06-02 19:24:22 +0930

    added datepicker widget

---
+++ crud/providers/DateProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace schmunk42\giiant\crud\providers;
+
+class DateProvider extends \schmunk42\giiant\base\Provider
+{
+    public function activeField($attribute)
+    {
+        $column = $this->generator->getTableSchema()->columns[$attribute->name];
+
+        switch (true) {
+            case (in_array($column->name, $this->columnNames)):
+                $this->generator->requires[] = 'zhuravljov/yii2-datetime-widgets';
+                return <<<EOS
+\$form->field(\$model, '{$column->name}')->widget(\zhuravljov\widgets\DatePicker::className(), [
+    'options' => ['class' => 'form-control'],
+    'clientOptions' => [
+        'autoclose' => true,
+        'todayHighlight' => true,
+    ],
+])
+EOS;
+                break;
+            default:
+                return null;
+        }
+    }
+} 
