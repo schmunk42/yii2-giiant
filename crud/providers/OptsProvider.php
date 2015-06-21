e4e7fa48ec1f520357f0d752f4be17c669670f95
Sergej Kunz <s.kunz@herzogkommunikation.de>
2015-06-21 09:34:28 +0200

    fixed #98

--- crud/providers/OptsProvider.php
+++ crud/providers/OptsProvider.php
@@ -10,37 +10,37 @@ use yii\db\ColumnSchema;
  */
 class OptsProvider extends \schmunk42\giiant\base\Provider
 {
-    public function activeField($attribute)
-    {
-        $column     = $this->generator->getColumnByAttribute($attribute);
-        if (!$column) {
-            return null;
-        }
+	public function activeField($attribute)
+	{
+		$column = $this->generator->getColumnByAttribute($attribute);
+		if (!$column) {
+			return null;
+		}
 
-        $modelClass = $this->generator->modelClass;
-        $func       = 'opts' . str_replace("_", "", $attribute);
-        $camel_func = 'opts' . str_replace(' ', '', ucwords(implode(' ', explode('_', $attribute))));
-        
-        if (method_exists($modelClass::className(), $func)) {
-            $mode = isset($this->columnNames[$attribute]) ? $this->columnNames[$attribute] : null;
-        }elseif (method_exists($modelClass::className(), $camel_func)) {
-            $func = $camel_func;
-            $mode = isset($this->columnNames[$attribute]) ? $this->columnNames[$attribute] : null;
-        } else {
-            return null;
-        }
+		$modelClass = $this->generator->modelClass;
+		$func = 'opts' . str_replace("_", "", $attribute);
+		$camel_func = 'opts' . str_replace(' ', '', ucwords(implode(' ', explode('_', $attribute))));
 
-        switch ($mode) {
-            case 'radio':
-                return <<<EOS
+		if (method_exists($modelClass::className(), $func)) {
+			$mode = isset($this->columnNames[$attribute]) ? $this->columnNames[$attribute] : null;
+		} elseif (method_exists($modelClass::className(), $camel_func)) {
+			$func = $camel_func;
+			$mode = isset($this->columnNames[$attribute]) ? $this->columnNames[$attribute] : null;
+		} else {
+			return null;
+		}
+
+		switch ($mode) {
+			case 'radio':
+				return <<<EOS
                     \$form->field(\$model, '{$attribute}')->radioList(
                         {$modelClass}::{$func}()
                     );
 EOS;
-                break;
+				break;
 
-            case 'select2':
-                return <<<EOS
+			case 'select2':
+				return <<<EOS
                     \$form->field(\$model, '{$attribute}')->widget(\kartik\select2\Select2::classname(), [
                         'name' => 'class_name',
                         'model' => \$model,
@@ -52,62 +52,62 @@ EOS;
                         ]
                     ]);
 EOS;
-                break;
+				break;
 
-            default:
-                // Render a dropdown list if the model has a method optsColumn().
-                return <<<EOS
+			default:
+				// Render a dropdown list if the model has a method optsColumn().
+				return <<<EOS
                         \$form->field(\$model, '{$attribute}')->dropDownList(
                             {$modelClass}::{$func}()
                         );
 EOS;
 
-        }
+		}
 
-        return null;
+		return null;
 
-    }
-    
-    /**
-     * Formatter for detail view attributes, who have get[..]ValueLabel function
-     * @param $column ColumnSchema
-     * @return null|string
-     */
-    public function attributeFormat($attribute)
-    {
-        $modelClass = $this->generator->modelClass;
-        $camel_func = 'get' . str_replace(' ', '', ucwords(implode(' ', explode('_', $attribute)))).'ValueLabel';
-        
-        if (!method_exists($modelClass::className(), $camel_func)) {
-            return null;
-        }
+	}
 
-        return <<<EOS
+	/**
+	 * Formatter for detail view attributes, who have get[..]ValueLabel function
+	 * @param $column ColumnSchema
+	 * @return null|string
+	 */
+	public function attributeFormat($attribute)
+	{
+		$modelClass = $this->generator->modelClass;
+		$camel_func = 'get' . str_replace(' ', '', ucwords(implode(' ', explode('_', $attribute)))) . 'ValueLabel';
+
+		if (!method_exists($modelClass::className(), $camel_func)) {
+			return null;
+		}
+
+		return <<<EOS
             [
                 'attribute'=>'{$attribute}',
                 'value'=>{$modelClass}::{$camel_func}(\$model->{$attribute}),
             ]
 EOS;
-    }    
-    
-    /**
-    * Formatter for detail view attributes, who have get[..]ValueLabel function
-     *
-     * @param $column ColumnSchema
-     * @param $model ActiveRecord
-     *
-     * @return null|string
-     */
-    public function columnFormat($attribute, $model)
-    {
-        $modelClass = $this->generator->modelClass;
-        $camel_func = 'get' . str_replace(' ', '', ucwords(implode(' ', explode('_', $attribute)))).'ValueLabel';
-        
-        if (!method_exists($modelClass::className(), $camel_func)) {
-            return null;
-        }
+	}
 
-        return <<<EOS
+	/**
+	 * Formatter for detail view attributes, who have get[..]ValueLabel function
+	 *
+	 * @param $column ColumnSchema
+	 * @param $model ActiveRecord
+	 *
+	 * @return null|string
+	 */
+	public function columnFormat($attribute, $model)
+	{
+		$modelClass = $this->generator->modelClass;
+		$camel_func = 'get' . str_replace(' ', '', ucwords(implode(' ', explode('_', $attribute)))) . 'ValueLabel';
+
+		if (!method_exists($modelClass::className(), $camel_func)) {
+			return null;
+		}
+
+		return <<<EOS
 			[
                 'attribute'=>'{$attribute}',
                 'value' => function (\$model) {
@@ -117,8 +117,6 @@ EOS;
 EOS;
 
 
+	}
 
-        
-    }
-    
 }
