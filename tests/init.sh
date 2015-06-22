404b51e45b5fe1d00b354308160e91700d80c76d
Tobias Munk <schmunk@usrbin.de>
2015-06-22 21:09:36 +0200

    don't remove containers

--- tests/init.sh
+++ tests/init.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-# clean (kill and remove containers in stack)
+# cleanup
 docker-compose kill
-docker-compose rm -f
+#docker-compose rm -f
 
 # default inititalization of large databases hit timeout when running `./yii app/create-mysql-db`, so bring up the database first and sleep a while
 # TODO: update command with timeout ENV variable or param
