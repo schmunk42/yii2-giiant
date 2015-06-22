c6080c737e7d54662203392b30ae3255ee603dfb
Tobias Munk <schmunk@usrbin.de>
2015-06-22 21:47:47 +0200

    debug CI

--- tests/init.sh
+++ tests/init.sh
@@ -1,20 +1,18 @@
 #!/usr/bin/env bash
 
+# default inititalization of large databases hit timeout when running `./yii app/create-mysql-db`, so bring up the database first
 set +e
-docker run --name runner-example-databases -e MARIADB_PASS=veryveryverysecretadmin schmunk42/mariadb-example-databases
+    # don't throw an error here
+    # TODO: remove tlsverify=0 (Docker 1.7 issue)
+    docker --tlsverify=0 run -d --name runner-example-databases -e MARIADB_PASS=secretadmin schmunk42/mariadb-example-databases
 set -e
-docker start runner-example-databases
+docker --tlsverify=0 start runner-example-databases
 
 # cleanup
 docker-compose kill
 #docker-compose rm -f
 
-# default inititalization of large databases hit timeout when running `./yii app/create-mysql-db`, so bring up the database first and sleep a while
-# TODO: update command with timeout ENV variable or param
-#docker-compose up -d mariadb
-#sleep 45
-
 # start test stack
 docker-compose run --rm appcli \
-    sh -c './yii app/create-mysql-db && ./yii migrate'
+    sh -c './yii app/create-mysql-db && ./yii migrate --interactive=0'
 docker-compose up -d
