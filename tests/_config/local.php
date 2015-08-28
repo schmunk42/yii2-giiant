c5ea6edb2eb4acc4e10f3602ed713ee27b11f7e1
Tobias Munk <schmunk@usrbin.de>
2015-08-28 22:00:47 +0200

    updated test config

--- tests/_config/local.php
+++ tests/_config/local.php
@@ -3,16 +3,20 @@ return [
     /*'components'  => [
         'db'           => [
             'class'       => 'yii\db\Connection',
-            'dsn'         => getenv('DATABASE_DSN'),
+            'dsn'         => getenv('DATABASE_DSN_BASE').';dbname=employees', // DATABASE_DSN_DB
             'username'    => getenv('DATABASE_USER'),
             'password'    => getenv('DATABASE_PASSWORD'),
             'charset'     => 'utf8',
             'tablePrefix' => getenv('DATABASE_TABLE_PREFIX'),
         ],
     ],*/
-    /* 'modules' => [
-         'crud' => [
-             'class'  => 'app\modules\crud\Module',
+    /*'modules' => [
+         'sakila' => [
+             'class'  => 'app\modules\sakila\Module',
+             'layout' => '@admin-views/layouts/main',
+         ],
+         'employees' => [
+             'class'  => 'app\modules\employees\Module',
              'layout' => '@admin-views/layouts/main',
          ],
      ]*/
