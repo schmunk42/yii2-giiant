6468c72cd00355eb80cac3badc4a8ef8eff2b1cf
Sebastián Thierer <sebas@artfos.com>
2015-11-25 12:14:59 -0300

    Update form.php

--- model/form.php
+++ model/form.php
@@ -11,10 +11,10 @@ echo $form->field($generator, 'modelClass');
 echo $form->field($generator, 'ns');
 echo $form->field($generator, 'baseClass');
 echo $form->field($generator, 'db');
-echo $form->field($generator, 'generateRelations')->dropDownList([
-    Generator::RELATIONS_NONE => Yii::t('yii', 'No relations'),
-    Generator::RELATIONS_ALL => Yii::t('yii', 'All relations'),
-    Generator::RELATIONS_ALL_INVERSE => Yii::t('yii', 'All relations with inverse'),
+echo $form->field($generator, 'generateRelations')->dropDownList([    
+    \yii\gii\generators\model\Generator::RELATIONS_NONE => Yii::t('yii', 'No relations'),
+    \yii\gii\generators\model\Generator::RELATIONS_ALL => Yii::t('yii', 'All relations'),
+    \yii\gii\generators\model\Generator::RELATIONS_ALL_INVERSE => Yii::t('yii', 'All relations with inverse'),
 ]);
 echo $form->field($generator, 'generateLabelsFromComments')->checkbox();
 echo $form->field($generator, 'generateModelClass')->checkbox();
