34a315a94282d13925945634bff44ca4f0b9fd6c
Sebastián Thierer <sebas@artfos.com>
2015-11-25 12:10:57 -0300

    Update form.php

--- model/form.php
+++ model/form.php
@@ -11,7 +11,11 @@ echo $form->field($generator, 'modelClass');
 echo $form->field($generator, 'ns');
 echo $form->field($generator, 'baseClass');
 echo $form->field($generator, 'db');
-echo $form->field($generator, 'generateRelations')->checkbox();
+echo $form->field($generator, 'generateRelations')->dropDownList([
+    Generator::RELATIONS_NONE => Yii::t('yii', 'No relations'),
+    Generator::RELATIONS_ALL => Yii::t('yii', 'All relations'),
+    Generator::RELATIONS_ALL_INVERSE => Yii::t('yii', 'All relations with inverse'),
+]);
 echo $form->field($generator, 'generateLabelsFromComments')->checkbox();
 echo $form->field($generator, 'generateModelClass')->checkbox();
 echo $form->field($generator, 'enableI18N')->checkbox();
