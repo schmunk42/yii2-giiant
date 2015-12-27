df56dd3335d97f1519de9690b0ca135f58ef0b9d
Tobias Munk <schmunk@usrbin.de>
2015-12-28 00:59:06 +0100

    added access control to rest/api controllers

--- src/generators/crud/default/controller-rest.php
+++ src/generators/crud/default/controller-rest.php
@@ -12,7 +12,29 @@ namespace <?= $generator->controllerNs ?>\api;
 /**
  * This is the class for REST controller "<?= $controllerClassName ?>".
  */
+
+use yii\filters\AccessControl;
+
 class <?= $controllerClassName ?> extends \yii\rest\ActiveController
 {
     public $modelClass = '<?= $generator->modelClass ?>';
+<?php if ($generator->accessFilter): ?>
+    /**
+    * @inheritdoc
+    */
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'rules' => [
+                    [
+                        'allow' => true,
+                        'matchCallback' => function ($rule, $action) {return \Yii::$app->user->can($this->module->id . '_' . $this->id . '_' . $action->id, ['route' => true]);},
+                    ]
+                ]
+            ]
+        ];
+    }
+<?php endif; ?>
 }
