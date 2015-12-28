efce93a890ef33ea002b4e2f314391a971885d5e
Tobias Munk <schmunk@usrbin.de>
2015-12-28 02:55:55 +0100

    improved test stack isolation

--- tests/run.sh
+++ tests/run.sh
@@ -4,9 +4,13 @@ set -e
 
 export GIIANT_TEST_DB=${GIIANT_TEST_DB-sakila}
 export CI_APP_VOLUME=${CI_APP_VOLUME-..}
+export COMPOSE_PROJECT_NAME=${COMPOSE_PROJECT_NAME-giiant}
 DOCKER_COMPOSE="docker-compose --x-networking"
 
-docker exec tests_php_1 sh -c "\
+# replace the stack name in the yml configuration for isolated CI stacks
+sed -i.bak s/giiant_/${COMPOSE_PROJECT_NAME}_/ acceptance.suite.yml
+
+docker exec ${COMPOSE_PROJECT_NAME}_php_1 sh -c "\
     codecept clean -c /app/vendor/schmunk42/yii2-giiant/codeception.yml && \
     codecept run -c /app/vendor/schmunk42/yii2-giiant/codeception.yml cli prod/base && \
     codecept run -c /app/vendor/schmunk42/yii2-giiant/codeception.yml cli prod/${GIIANT_TEST_DB} && \
