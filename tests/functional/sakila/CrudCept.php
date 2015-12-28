6f54b2df4dd87066bb6d761e8a966d65fb3af057
Tobias Munk <schmunk@usrbin.de>
2015-12-28 01:24:09 +0100

    fixed functional test

--- tests/functional/sakila/CrudCept.php
+++ tests/functional/sakila/CrudCept.php
@@ -5,5 +5,5 @@ use schmunk42\giiant\tests\_pages\LoginPage;
 $I = new FunctionalTester($scenario);
 
 $I->wantTo('ensure that crud works (WITHOUT access rules applied!)');
-$I->amOnPage('/crud/film');
+$I->amOnPage('/en/crud/film');
 $I->see('Films', 'h1');
