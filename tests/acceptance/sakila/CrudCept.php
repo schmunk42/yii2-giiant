769e60125e55e2024ae3cdb50261d35cd10bb672
Tobias Munk <schmunk@usrbin.de>
2016-03-09 00:05:41 +0100

    fixed test breaks

--- tests/acceptance/sakila/CrudCept.php
+++ tests/acceptance/sakila/CrudCept.php
@@ -4,8 +4,6 @@
 
 use tests\_pages\LoginPage;
 
-#new schmunk42\giiant\tests\_pages\LoginPage;
-
 $I = new AcceptanceTester($scenario);
 
 $I->wantTo('ensure that crud works with access rules');
@@ -16,16 +14,9 @@ $I->makeScreenshot('crud-actor-login');
 $I->see('Sign in', 'h3');
 $I->amGoingTo('try to login with correct credentials');
 
-// TODO: use LoginPage
-$username = 'admin';
-$password = 'admin';
-$I->fillField('input[name="login-form[login]"]', $username);
-$I->fillField('input[name="login-form[password]"]', $password);
-$I->click('Sign in');
-$I->waitForElement('.user-footer .pull-right a', 5);
-#LoginPage::openBy($I);
-#$loginPage = LoginPage::openBy($I);
-#$loginPage->login('admin', 'admin');
+LoginPage::openBy($I);
+$loginPage = LoginPage::openBy($I);
+$loginPage->login('admin', 'admin');
 
 $I->see('Actors', 'h1');
 $I->makeScreenshot('crud-actor');
