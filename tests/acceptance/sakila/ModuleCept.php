144c143011e0074a82a8490c3643fad0797d976e
Tobias Munk <schmunk@usrbin.de>
2016-05-09 15:04:42 +0200

    enabled access behavior in generated giiant module

--- tests/acceptance/sakila/ModuleCept.php
+++ tests/acceptance/sakila/ModuleCept.php
@@ -2,10 +2,23 @@
 
 // @group sakila
 
-use schmunk42\giiant\tests\_pages\LoginPage;
+use tests\_pages\LoginPage;
 
 $I = new AcceptanceTester($scenario);
+
+$I->wantTo('ensure that crud works with access rules');
+$I->amOnPage('/sakila');
+$I->dontSee('Actors', 'h1');
+$I->makeScreenshot('module-login');
+
+$I->see('Sign in', 'h3');
+$I->amGoingTo('try to login with correct credentials');
+LoginPage::openBy($I);
+$loginPage = LoginPage::openBy($I);
+$loginPage->login('admin', 'admin');
+
 
 $I->wantTo('ensure that module works');
 $I->amOnPage('/sakila');
 $I->see('rental');
+$I->makeScreenshot('module-index');
