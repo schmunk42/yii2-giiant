ca0f89d49ab64a90eda763f8137c137ca165475f
Tobias Munk <schmunk@usrbin.de>
2016-05-09 13:03:35 +0200

    hardened tests

--- tests/acceptance/onlyCrud/CrudCept.php
+++ tests/acceptance/onlyCrud/CrudCept.php
@@ -6,22 +6,39 @@ use tests\_pages\LoginPage;
 
 $I = new AcceptanceTester($scenario);
 
-$I->wantTo('ensure that crudOnly works with access rules');
+$I->wantTo('ensure that crudOnly works (no access rules)');
+
 $I->amOnPage('/onlyCrud/country');
-$I->dontSee('Actors', 'h1');
-$I->makeScreenshot('crud-country-login');
-
-$I->see('Sign in', 'h3');
-$I->amGoingTo('try to login with correct credentials');
-
-LoginPage::openBy($I);
-$loginPage = LoginPage::openBy($I);
-$loginPage->login('admin', 'admin');
-
-$I->see('Actors', 'h1');
+$I->see('Countries', 'h1');
 $I->makeScreenshot('crud-country');
 
 $I->expectTo('see view, edit and delete button');
 $I->seeElementInDOM('[data-key="1"] .glyphicon-eye-open');
 $I->seeElementInDOM('[data-key="2"] .glyphicon-pencil');
 $I->seeElementInDOM('[data-key="3"] .glyphicon-trash');
+
+$I->amGoingTo('create a Country');
+$I->click('New');
+
+$I->see('Country', 'h1');
+$I->see('Create', '.btn');
+
+$I->fillField('#country-country', 'Xyzland');
+$I->makeScreenshot('crud-pre-create-country');
+$I->click('Create');
+$I->wait(1);
+
+$I->see('Xyzland', 'table');
+$I->see('Country', 'table');
+$I->makeScreenshot('crud-create-country');
+
+
+$I->click('Edit');
+$I->wait(1);
+
+$I->see('Xyzland', 'input');
+$I->see('Country', 'h1');
+$I->fillField('#country-country', 'Abcstan');
+$I->click('Save');
+
+$I->makeScreenshot('crud-edit-country');
