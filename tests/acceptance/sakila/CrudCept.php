83f042402986dc220fe17d280371450905b736b8
Alex Pogorelov <shurik2k5@gmail.com>
2019-05-20 21:36:40 +0300

    fix tests

--- tests/acceptance/sakila/CrudCept.php
+++ tests/acceptance/sakila/CrudCept.php
@@ -12,6 +12,6 @@ $I->comment('editor configured in test application');
 $I->amOnPage('/sakila/actor');
 
 $I->expectTo('see view, edit and delete button');
-$I->seeElementInDOM('[data-key="1"] .glyphicon-file');
+$I->seeElementInDOM('[data-key="1"] .glyphicon-eye-open');
 $I->seeElementInDOM('[data-key="2"] .glyphicon-pencil');
 $I->seeElementInDOM('[data-key="3"] .glyphicon-trash');
