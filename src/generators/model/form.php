0c2e711328708611245cf73dac2e75da4024fb6e
uldisn <uldisnelsons@gmail.com>
2021-02-02 21:02:14 +0200

    To  SaveForm helper methods added parameter $giiInfoPath

--- src/generators/model/form.php
+++ src/generators/model/form.php
@@ -12,10 +12,10 @@ use schmunk42\giiant\helpers\SaveForm;
  * on chenging listbox, form fill with selected saved forma data
  * currently work with input text, input checkbox and select form fields
  */
-$this->registerJs(SaveForm::getSavedFormsJs($generator->getName()), yii\web\View::POS_END);
+$this->registerJs(SaveForm::getSavedFormsJs($generator->getName(), $generator->giiInfoPath), yii\web\View::POS_END);
 $this->registerJs(SaveForm::jsFillForm(), yii\web\View::POS_END);
 echo $form->field($generator, 'savedForm')->dropDownList(
-        SaveForm::getSavedFormsListbox($generator->getName()), ['onchange' => 'fillForm(this.value)']
+        SaveForm::getSavedFormsListbox($generator->getName() , $generator->giiInfoPath), ['onchange' => 'fillForm(this.value)']
 );
 
 echo $form->field($generator, 'tableName');
